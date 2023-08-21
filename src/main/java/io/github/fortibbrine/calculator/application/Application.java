bf44d69472711ab72e4e23d50e921aa985353601
FortiBrine <FortiBrine@users.noreply.github.com>
2023-08-21 10:01:15 +0300

    check how to event handle click and CSS style

--- src/main/java/io/github/fortibbrine/calculator/application/Application.java
+++ src/main/java/io/github/fortibbrine/calculator/application/Application.java
@@ -1,7 +1,14 @@
 package io.github.fortibbrine.calculator.application;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
@@ -15,11 +22,24 @@ public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Text text = new Text("hello world");
-        text.setFont(new Font(20));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(text);
+        HBox root = new HBox();
+
+        root.setSpacing(20);
+
+        Button button1 = new Button("hello world");
+        Button button2 = new Button("hello");
+
+        button1.setFont(new Font(30));
+        button2.setFont(new Font(30));
+
+        button1.setOnMouseClicked(event -> System.out.println("?"));
+
+        button1.setStyle("-fx-background-color: white");
+        button2.setStyle("-fx-background-color: white");
+
+        root.getChildren().add(button1);
+        root.getChildren().add(button2);
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
